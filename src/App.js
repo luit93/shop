b7dfd9f627ee4f6b53ff487e6cc1194d864598f7
luit saikia <luit93@gmail.com>
2022-04-17 04:09:22 +1000

    login page added

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import ProductPage from './pages/ProductPage';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div class="app-body">
     <Layout >
-      <ProductPage/>
-      <Register/>
       
+    <Login/>
+      <Register/>
     </Layout>
   
      
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,42 +6,52 @@ height: 100vh;
 background: linear-gradient(rgba(255,255,255,0.3),rgba(255,255,255,0.4)), url("https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/rm218batch2-nunny-20.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=5b755b72ab40dfc1f9a810f3497db357")  center  ;
 background-size:cover;
 display: flex;
+flex-direction: column;
 align-items: center;
 justify-content: center;
 `
 
 const Wrapper = styled.div`
 padding: 20px;
-width: 40%;
+width: 25%;
 background-color: aliceblue;
 display: flex;
 flex-direction: column;
 align-items: center;
 justify-content: center;
+padding: 20px 10px;
 `
 
 const Title = styled.h1`
-font-size: 25px;
-font-weight: 400;
+font-size: 35px;
+font-weight: 300;
 `
 
 const Form = styled.form`
 display: flex;
-flex-wrap: wrap;
+flex-direction: column;
 padding: 20px;
+max-width: 50%;
+align-items: center;
+justify-content: center;
 
 `
 
 const Input = styled.input`
-min-width: 40%;
+/* min-width: 40%; */
 flex:1;
-margin: 10px 10px 0 0;
+margin: 10px  0;
 padding: 10px;
 `
 
-const Agreement = styled.div`
-font-size:24px;
-margin: 20px 0;
+
+
+const Link = styled.a`
+font-size:12px;
+margin: 5px 0;
+text-decoration: underline;
+cursor: pointer;
+
 `
 
 const Button = styled.button`
@@ -51,34 +61,32 @@ border: none;
 padding: 20px 20px;
 background-color: teal;
 color: white;
+/* margin-bottom: 10px; */
+height: 30px;
+width: 90px;
+
 `
 
-const Link = styled.a`
-font-size:12px;
-margin: 20px 0;
-padding: 5px 20px;
-`
 
 
 const Register = () => {
   return (
     <Container>
         <Wrapper>
-            <Title>Create an Account</Title>
+            <Title>Log In</Title>
             <Form>
-                <Input placeholder="first name"/>
-                <Input placeholder="last name"/>
+               
                 <Input placeholder="username"/>
-                <Input placeholder="email"/>
+                
                 <Input placeholder="passwords"/>
-                <Input placeholder="confirm password"/>
-                <Agreement>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ea.
-                </Agreement>
-                <Button>Register</Button>
-                <Link>Already Registered? Log in to your Account</Link>
-
-            </Form>
+                <Button>Log In</Button>
+                </Form>
+              </Wrapper>
+              <Wrapper>
+                
+                <Link>Forgot Password?</Link>
+                <Link>Not Registered? Create an Account</Link>
+           
         </Wrapper>
     </Container>
   )
